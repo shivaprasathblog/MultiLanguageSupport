4c7c1bc496a5b5918a0ace5316bd47f9c01e3a05
Shivaprasath <shiva.github@gmail.com>
2015-03-18 17:39:18 +0530

    Multi Language Support
    This is simple piece of code to understand Multilanguage support , here
    i have supported English and French , as soon you change your language
    preference to French , the app resources will also change to French.

---
+++ src/com/example/multilanguagesupport/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.multilanguagesupport;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity 
+{
+
+	/**Add folder under res folder with the name values-fr and in that add strings.xml , 
+	add french resources (french support)if u want your app to support both English and French ,
+	Likewise if you want to support any other language like  Portuguese add folder under res as 
+	values- pt and in that add strings.xml , add Portuguese resources (Portuguese support)**/
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) 
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
