670c05ebcc90e0d850db17bcc3c81ac038b4490a
Shivaprasath <shivaprasathblog@users.noreply.github.com>
2015-03-18 17:43:30 +0530

    Update MainActivity.java

--- src/com/example/multilanguagesupport/MainActivity.java
+++ src/com/example/multilanguagesupport/MainActivity.java
@@ -7,10 +7,13 @@ import android.view.Menu;
 public class MainActivity extends Activity 
 {
 
+//Change Language Preference in Phone (or) Emulator Setting to see the effect .
+
 	/**Add folder under res folder with the name values-fr and in that add strings.xml , 
 	add french resources (french support)if u want your app to support both English and French ,
 	Likewise if you want to support any other language like  Portuguese add folder under res as 
 	values- pt and in that add strings.xml , add Portuguese resources (Portuguese support)**/
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
